726c123836be40a659dad2e6df4a03f854e3098f
Panjoy1520 <pranjaydwivedi01@gmail.com>
2025-12-06 10:46:10 +0530

    add config files

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,8 @@
 export const authConfig = {
   clientId: 'oauth2-pkce-client',
-  authorizationEndpoint: 'http://localhost:8181/realms/fitness-app/protocol/openid-connect/auth',
-  tokenEndpoint: 'http://localhost:8181/realms/fitness-app/protocol/openid-connect/token',
-  redirectUri: 'http://localhost:5173',
+  authorizationEndpoint: 'http://35.154.160.83:8181/realms/fitness-app/protocol/openid-connect/auth',
+  tokenEndpoint: 'http://35.154.160.83:8181/realms/fitness-app/protocol/openid-connect/token',
+  redirectUri: 'https://fit-ai-frontend.vercel.app',
   scope: 'openid profile email offline_access',
   onRefreshTokenExpire: (event) => event.logIn(),
 }
